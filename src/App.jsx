15bdb2c72d7b87f468a440b25a6d67ce06c6e411
raj <gomeshwarr@gmail.com>
2022-01-04 15:56:30 +0530

    voice module
    voice module for bot

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
 
   return (
     <section className="voice-recorder">
-      <h1 className="title">Voice Recorder</h1>
+      <h1 className="title">Bot Voice Recorder Module</h1>
       <div className="recorder-container">
         <RecorderControls recorderState={recorderState} handlers={handlers} />
         <RecordingsList audio={audio} />
